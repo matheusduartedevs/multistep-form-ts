9fb0062dead0ad9959b93749e92ecb7c95e0fea2
unknown <matheus.duartesilva42@gmail.com>
2023-07-04 15:55:07 -0300

    implementação do primeiro passo: interligação com o FormContext

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useEffect } from 'react'
+import { FormActions, useForm } from '../contexts/FormContext'
 import { useNavigate } from 'react-router-dom'
 import Theme from '../../components/Theme'
 import * as C from './styles'
@@ -5,8 +7,28 @@ import * as C from './styles'
 const FormStep1 = () => {
   const navigate = useNavigate()
 
+  const { state, dispatch } = useForm()
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    })
+  }, [])
+
   const handleNextStep = () => {
-    navigate('/step2')
+    if (state.name !== '') {
+      navigate('/step2')
+    } else {
+      alert('Preencha os dados!')
+    }
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    })
   }
 
   return (
@@ -22,6 +44,8 @@ const FormStep1 = () => {
           <input
             type="text"
             autoFocus 
+            value={state.name}
+            onChange={handleNameChange}
           />
         </label>
 
