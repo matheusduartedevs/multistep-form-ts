52486e411e367e5eadb162b78ded1de4b5477415
unknown <matheus.duartesilva42@gmail.com>
2023-07-04 15:31:14 -0300

    desenvolvimento do primeiro passo do form

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,31 @@
+import { useNavigate } from 'react-router-dom'
 import Theme from '../../components/Theme'
 import * as C from './styles'
 
 const FormStep1 = () => {
+  const navigate = useNavigate()
+
+  const handleNextStep = () => {
+    navigate('/step2')
+  }
+
   return (
     <Theme>
       <C.Container>
-        Passo 1
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+        <hr />
+
+        <label>
+          Seu nome completo
+          <input
+            type="text"
+            autoFocus 
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
   )
