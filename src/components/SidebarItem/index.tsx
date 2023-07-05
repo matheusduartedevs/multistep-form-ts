894b33fe7dc18b57f0f5c9446ce7ba26e66980ec
unknown <matheus.duartesilva42@gmail.com>
2023-07-05 08:52:46 -0300

    finalização da sidebar

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -4,22 +4,19 @@ import * as C from './styles'
 type Props = {
     title: string
     description: string
-    icon: string
     path: string
+    active: boolean
 }
 
-const SidebarItem = ({ title, description, icon, path }: Props) => {
+const SidebarItem = ({ title, description, path, active }: Props) => {
     return (
-        <C.Container>
+        <C.Container active={active}>
             <Link to={path}>
                 <C.Info>
                     <C.Title>{title}</C.Title>
                     <C.Description>{description}</C.Description>
                 </C.Info>
-                <C.IconArea>
-
-                </C.IconArea>
-                <C.Point></C.Point>
+                <C.Point active={active}></C.Point>
             </Link>
         </C.Container>
     )
--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -2,38 +2,43 @@ import { ReactNode } from 'react'
 import Header from '../Header'
 import * as C from './styles'
 import SidebarItem from '../SidebarItem'
+import { useForm } from '../../pages/contexts/FormContext'
 
 type Props = {
     children: ReactNode
 }
 
 const Theme = ({ children }: Props) => {
+    const { state } = useForm()
+
     return (
         <C.Container>
             <C.Area>
                 <Header />
 
                 <C.Steps>
-                    <SidebarItem
-                        title='Pessoal'
-                        description='Se identifique'
-                        icon='profile'
-                        path='/'
-                    />
+                    <C.Sidebar>
+                        <SidebarItem
+                            title='Pessoal'
+                            description='Se identifique'
+                            path='/'
+                            active={state.currentStep === 1}
+                        />
 
-                    <SidebarItem
-                        title='Profissional'
-                        description='Seu nível'
-                        icon='book'
-                        path='/step2'
-                    />
+                        <SidebarItem
+                            title='Profissional'
+                            description='Seu nível'
+                            path='/step2'
+                            active={state.currentStep === 2}
+                        />
 
-                    <SidebarItem
-                        title='Contatos'
-                        description='Como te achar'
-                        icon='mail'
-                        path='/step3'
-                    />
+                        <SidebarItem
+                            title='Contatos'
+                            description='Como te achar'
+                            path='/step3'
+                            active={state.currentStep === 3}
+                        />
+                    </C.Sidebar>
                     <C.Page>
                         {children}
                     </C.Page>
